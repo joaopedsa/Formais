6e4913a60f8f81b1823276d5b83bb510b6694b26
joaopedsa <joaopedrosantana1997@hotmail.com.br>
2019-04-22 20:07:52 -0300

    AJAIO

--- src/automato.js
+++ src/automato.js
@@ -1,3 +1,5 @@
+const Transition = require('./transition');
+
 module.exports = class Automato {
     
     constructor(states,alphabet,transitions,initial,finals) {
@@ -9,16 +11,26 @@ module.exports = class Automato {
     }
 
     isDeterministic() {
-        for(let i = 0; i < this.transitions.length ; ++i) {
-            if(this.transitions[i].to.includes(',')) return false
-        }
+        for(let i = 0; i < this.transitions.length - 1; ++i)
+            if(this.transitions[i].to.includes(','))
+                return false
         return true
     }
 
     determinize() {
-        if(isDeterministic()) {
-            
-        }
+        // Verifica se o automato é deterministico
+        if(!this.isDeterministic())
+            // percorro o laço em busca da transição não deterministica
+            for(let i = 0; i < this.transitions.length; ++i) {
+                if(!this.transitions[i].to.includes(','))
+                    continue    
+                // atribuindo valor da transição não deterministica na constante from    
+                const from = this.transitions[i].to
+                for(let j = 0 ; j < this.transitions.length; ++j) {
+                    if(!from.includes(this.transitions[j].from))
+                        continue
+                    /*Criação das transições novas e agora no laço do alfabeto*/
+                } 
+            }
     }
-
 }
--- src/test/testAutomato.js
+++ src/test/testAutomato.js
@@ -7,7 +7,7 @@ function test () {
     const states = [ 'q0', 'q1', 'q2', 'q3', 'q4' ]
     const alphabet = [ 'a', 'b' ]
     const transitions = [
-    new Transition('q0', 'q1', 'a'),
+    new Transition('q0', 'q1,q2', 'a'),
     new Transition('q0', 'q1', 'b'),
     new Transition('q2', 'q4', 'a'),
     new Transition('q2', 'q0', 'b')
@@ -15,6 +15,6 @@ function test () {
     const start = 'q0'
     const final = [ 'q4' ]
     automato = new Automato(states, alphabet, transitions, start, final)
-    console.log(automato.isDeterministic())
+    console.log(automato.determinize())
 }
 test()
