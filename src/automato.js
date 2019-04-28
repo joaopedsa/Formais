e14154bc1d1e45d7c7fdbfa22f98e735d774bd12
joaopedsa <joaopedrosantana1997@hotmail.com.br>
2019-04-28 02:26:52 -0300

    WIP

--- src/automato.js
+++ src/automato.js
@@ -10,27 +10,74 @@ module.exports = class Automato {
         this.finals = finals
     }
 
+    determinize() {
+        const fecho = this.fechoTransitivo()
+        // Verifica se o automato é deterministico
+        if(!this.isDeterministic()) {
+            const posEpsilon = this.alphabet.indexOf('&')
+            if(posEpsilon !== -1) {
+                let newAlphabet = this.alphabet;
+                newAlphabet.splice(posEpsilon,1)
+                let newAutomato = new Automato([fecho[0]],newAlphabet,[],fecho[0],)
+                newAutomato.states.forEach(state => {
+                    let states = state.split(',')
+                    fecho.forEach(caminho => {
+                        /*Falta coisa pra caralho amanha é um novo dia*/
+                    })
+                })
+                
+
+            } else {
+
+            }
+
+        }
+    }
+
     isDeterministic() {
+        if(this.alphabet.indexOf('&') > -1)
+            return false
         for(let i = 0; i < this.transitions.length - 1; ++i)
             if(this.transitions[i].to.includes(','))
                 return false
         return true
     }
 
-    determinize() {
-        // Verifica se o automato é deterministico
-        if(!this.isDeterministic())
-            // percorro o laço em busca da transição não deterministica
-            for(let i = 0; i < this.transitions.length; ++i) {
-                if(!this.transitions[i].to.includes(','))
-                    continue    
-                // atribuindo valor da transição não deterministica na constante from    
-                const from = this.transitions[i].to
-                for(let j = 0 ; j < this.transitions.length; ++j) {
-                    if(!from.includes(this.transitions[j].from))
-                        continue
-                    /*Criação das transições novas e agora no laço do alfabeto*/
-                } 
+    fechoTransitivo() {
+        let fecho = []
+        if(this.alphabet.indexOf('&') === -1) {
+            this.transitions.forEach(transition => {
+                if(fecho.indexOf(transition.from) === -1)
+                    fecho = [...fecho, transition.from]
+            })
+        } else {
+            let newTransitions = this.transitions.filter(transition => transition.symbol === '&')
+            newTransitions.forEach((transition, index) => {
+                if(transition.to.includes(transition.from)) {
+                    fecho = [...fecho, transition.to]
+                } else {
+                    let newTransition = transition.to.split(',')
+                    newTransition.splice(index,0,transition.from)
+                    fecho = [...fecho,newTransition.join(',')]
+                }
+            })
+        }
+        fecho = this.organizaOrdem(fecho)
+        return fecho
+    }
+
+    organizaOrdem(states) {
+        states = states.map(state => {
+            let newState = ''
+            let stateSplit = state.split(',')
+            for(let i = 0; i < this.states.length; i++) {
+                if(stateSplit.indexOf('q' + i) !== -1 ) {
+                    if(newState) newState = newState +','+ stateSplit[stateSplit.indexOf('q' + i)]
+                    else newState = stateSplit[stateSplit.indexOf('q' + i)]
+                }
             }
+            return newState
+        }) 
+        return states;
     }
 }
--- src/test/testAutomato.js
+++ src/test/testAutomato.js
@@ -4,17 +4,25 @@ const Transition = require('../transition')
 
 function test () {
     let automato = null
-    const states = [ 'q0', 'q1', 'q2', 'q3', 'q4' ]
-    const alphabet = [ 'a', 'b' ]
+    const states = [ 'q0', 'q1', 'q2' ]
+    const alphabet = [ 'a', 'b', 'c', '&' ]
     const transitions = [
-    new Transition('q0', 'q1,q2', 'a'),
+    new Transition('q0', 'q0,q1', '&'),
+    new Transition('q0', '', 'a'),
     new Transition('q0', 'q1', 'b'),
-    new Transition('q2', 'q4', 'a'),
-    new Transition('q2', 'q0', 'b')
+    new Transition('q0', 'q2', 'c'),
+    new Transition('q1', '', '&'),
+    new Transition('q1', 'q0', 'a'),
+    new Transition('q1', 'q2', 'b'),
+    new Transition('q1', 'q0,q1', 'c'),
+    new Transition('q2', '', '&'),
+    new Transition('q2', '', 'a'),
+    new Transition('q2', '', 'b'),
+    new Transition('q2', '', 'c')
     ]
     const start = 'q0'
-    const final = [ 'q4' ]
+    const final = [ 'q2' ]
     automato = new Automato(states, alphabet, transitions, start, final)
-    console.log(automato.determinize())
+    automato.determinize()
 }
 test()
