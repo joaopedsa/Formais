4193391c48c6016d8f4c368201df1cc85ebba183
João Pedro Santana <joaopedrosantana1997@hotmail.com.br>
2019-05-10 19:31:35 -0300

    WIP

--- Interface/src/components/af/af.js
+++ Interface/src/components/af/af.js
@@ -54,18 +54,3 @@ export default class af extends Component {
     )
   }
 }
-
-/*
-Swal.fire({
-  title: 'Custom width, padding, background.',
-  width: 600,
-  padding: '3em',
-  background: '#fff url(/images/trees.png)',
-  backdrop: `
-    rgba(0,0,123,0.4)
-    url("/images/nyan-cat.gif")
-    center left
-    no-repeat
-  `
-})
-*/
